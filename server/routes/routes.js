21e11db1d75693f4f554b3598d1234e33630ab75
Clint Kunz <clintkunz@gmail.com>
2018-10-29 13:57:44 -0600

    other models - find by id, remove, update

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,7 +4,7 @@ const models = require('../models/models.js');
 
 const router = express.Router();
 
-//Get
+//Get list of notes
 router.get('/notes', (req, res) => {
     models
         .getNotes()
@@ -13,5 +13,24 @@ router.get('/notes', (req, res) => {
             })
             .catch(err => res.status(500).json(err));
 });
+
+//Create a note with a title and content
+router.post('/create', (req, res) => {
+    const { title, content, user_id } = req.body;
+    const note_title = title;
+    const note_content = content; 
+    const note = { note_title, note_content, user_id };
+
+    if(!title && !content && !user_id) {
+        res.status(406).json({ error: 'req body not acceptable' });
+    } else {
+        models
+            .createNote(note)
+                .then(ids => {
+                    res.status(201).json(ids[0]);
+                })
+                .catch(err => res.status(500).json({ error: 'createNote failed', err }));
+    }
+})
 
 module.exports = router; 
