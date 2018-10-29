f94abd128b6102b0d05a91b113b3f3a91e15eb75
Clint Kunz <clintkunz@gmail.com>
2018-10-29 13:57:24 -0600

    create note with title content and user id

--- server/models/models.js
+++ server/models/models.js
@@ -3,9 +3,37 @@ const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
 
 module.exports = {
-    getNotes
+    getNotes,
+    createNote,
+    updateNote,
+    findNoteById,
+    removeNote
 };
 
 function getNotes() {
     return db('notes');
+};
+
+function createNote(note) {
+    return db('notes')
+        .insert(note)
+        .into('notes');
+};
+
+function updateNote(id, changes) {
+    return db('notes')
+      .where({ id })
+      .update(changes);
+};
+
+function findNoteById(id) {
+    return db('notes')
+      .where({ id })
+      .first();
+};
+
+function removeNote(id) {
+    return db('notes')
+      .where({ id })
+      .del();
 };
